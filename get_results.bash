9f7b3efb13ec8311dfad7dc1b78aee1e7874c722
Mathis003 <mathis.delsart@gmail.com>
2023-11-10 19:17:45 +0100

    Improve script bash + new graphs

--- get_results.bash
+++ get_results.bash
@@ -1,17 +1,25 @@
-OUTPUT_FILE="perfs.csv"
-TITLE_COLUMNS="NbThreads,Time1,Time2,Time3,Time4,Time5"
-NB_THREADS=(2 3 4 5 6 7 8)
-TIMES=()
+#!/bin/bash
 
+### Begin Variable ###
+OUTPUT_FILE="perfs.csv"
+NB_TIMES=10
+SEQ_NB_THREADS=(2 3 4 5 6 7 8)
+### End Variable ###
+
+TIMES=()
+TITLE_COLUMNS=$(printf "NbThreads%s" $(for ((i=1; i<=$NB_TIMES; i++)); do printf ",Time%d" $i; done))
 echo $TITLE_COLUMNS > $OUTPUT_FILE
+
 make -s clean
 make -s
-for i in {2..8}
+
+for i in "${SEQ_NB_THREADS[@]}";
 do
-    for j in {1..5}
+    for j in $(seq 1 $NB_TIMES)
     do
         TIME=$(/usr/bin/time -f %e ./main.out $i 2>&1 | tail -n 1)
-        TIMES[${j}-1]=${TIME}
+        TIMES[${j}-1]=$TIME
     done
-    echo "${NB_THREADS[${i}-1]},${TIMES[0]},${TIMES[1]},${TIMES[2]},${TIMES[3]},${TIMES[4]}" >> $OUTPUT_FILE
+    TIMES_LINE=$(printf "%d%s" $i $(for ((j=0; j<$NB_TIMES; j++)); do printf ",%s" "${TIMES[$j]}"; done))
+    echo $TIMES_LINE >> $OUTPUT_FILE
 done
