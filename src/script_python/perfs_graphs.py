67756522411ce97cde97f568ad123703b635191a
Mathis Delsart <mathis.delsart@student.uclouvain.be>
2023-11-28 17:47:05 +0100

    Boxplot graphs

--- src/script_python/perfs_graphs.py
+++ src/script_python/perfs_graphs.py
@@ -1,5 +1,6 @@
 from matplotlib import pyplot as plt
 import pandas as pd
+import numpy as np
 import sys
 import os
 
@@ -22,26 +23,30 @@ program_name = csv_file_name[6:]
 
 data = pd.read_csv(csv_file)
 nbThreads = data['NbThreads']
-data = data.drop(columns="NbThreads")
+data = data.drop('NbThreads', axis=1)
+data = data.values.transpose()
 
-medians = []
-means = []
-stds = []
-
-for index, row in data.iterrows():
-    medians.append(row.median())
-    means.append(row.mean())
-    stds.append(row.std())
+unity_time = "s"
+if np.max(data) < 1:
+    unity_time = "ms"
+    data *= 1000
 
 plt.figure()
-plt.plot(nbThreads, means, marker='o', markersize=6, color='blue', linestyle='-', label='Mean')
-plt.scatter(nbThreads, medians, color='green', label='Median', s=40)
-plt.errorbar(nbThreads, medians, yerr=stds, fmt='o', markersize=5, color='red', label='Std Dev')
+
+box = plt.boxplot(data, vert=True, meanline=True, showmeans=True, patch_artist=True)
+
+colors = ['lightblue', 'lightgreen', 'lightyellow', 'lightpink', 'lightgray']
+
+for patch, color in zip(box['boxes'], colors):
+    patch.set_facecolor(color)
+
+plt.xticks(np.arange(1, len(nbThreads) + 1), nbThreads)
+
 plt.title("Time Performance Metrics [{}.c] vs. Number of Threads".format(program_name))
 plt.xlabel('Number of Threads []')
-plt.ylabel('Execution Time [sec]')
+plt.ylabel('Execution Time [{}]'.format(unity_time))
 plt.legend()
 plt.grid()
 plt.savefig("graphs/png/" + csv_file_name + ".png")
 plt.savefig("graphs/pdf/" + csv_file_name + ".pdf")
-# plt.show()
+plt.show()
