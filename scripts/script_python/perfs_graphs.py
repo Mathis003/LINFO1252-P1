8caa8736f9c930ce63787d09dc449226122876e6
Mathis003 <mathis.delsart@student.uclouvain.be>
2023-12-03 22:37:59 +0100

    Add other graphs

--- scripts/script_python/perfs_graphs.py
+++ scripts/script_python/perfs_graphs.py
@@ -5,58 +5,74 @@ import sys
 import os
 
 if len(sys.argv) != 2:
-    print("Usage: python script.py <csv_file>")
+    print("Usage: name file <name_file>")
     sys.exit(1)
 
-csv_file = sys.argv[1]
+name_file = sys.argv[1]
+csv_files = ["./csv_files/perfs_" + name_file + "_TS.csv", "./csv_files/perfs_" + name_file + "_TTS.csv", "./csv_files/perfs_" + name_file + "_BTTS.csv"]
 
-if not os.path.isfile(csv_file):
-    print(f"Error: The file {csv_file} doesn't exist.")
-    sys.exit(1)
 
-if not csv_file.lower().endswith('.csv'):
-    print(f"Error: The file {csv_file} isn't a CSV file.")
-    sys.exit(1)
+datas = [pd.read_csv(csv_files[0]), pd.read_csv(csv_files[1]), pd.read_csv(csv_files[2])]
+nbThreads = datas[0]['NbThreads']
 
-csv_file_name = os.path.splitext(os.path.basename(csv_file))[0]
-program_name = csv_file_name[6:]
+means = []
+std_devs = []
 
-data = pd.read_csv(csv_file)
-nbThreads = data['NbThreads']
-data = data.drop('NbThreads', axis=1)
-data = data.values
+for i in range(3):
+    datas[i] = datas[i].drop('NbThreads', axis=1)
+    datas[i] = datas[i].values
 
-means = [np.mean(d) for d in data]
-std_devs = [np.std(d) for d in data]
+    means.append([np.mean(d) for d in datas[i]])
+    std_devs.append([np.std(d) for d in datas[i]])
 
-data = data.transpose()
+    datas[i] = datas[i].transpose()
 
-unity_time = "s"
-if np.max(data) < 1:
-    unity_time = "ms"
-    data *= 1000
-    means = np.multiply(means, 1000)
-    std_devs = np.multiply(std_devs, 1000)
 
 plt.figure()
 
-boxplot = plt.boxplot(data, vert=True, meanline=True, showmeans=True, patch_artist=True)
+colors = ['lightblue', 'lightgreen', 'lightpink', '', '', '', '']
 
-plt.errorbar(range(1, len(nbThreads) + 1), means, yerr=std_devs, fmt='ro', markersize=2, label='Standard deviation')
+for i in range(3):
+    color = colors[i]
 
-colors = ['lightblue', 'lightgreen', 'lightyellow', 'lightpink', 'lightgray']
+    boxplot = plt.boxplot(datas[i], vert=True, meanline=True, showmeans=True, patch_artist=True)
+    plt.errorbar(range(1, len(nbThreads) + 1), means[i], yerr=std_devs[i], fmt='ro', markersize=2, label='Standard deviation')
 
-for patch, color in zip(boxplot['boxes'], colors):
-    patch.set_facecolor(color)
+    for patch, unused in zip(boxplot['boxes'], colors):
+        patch.set_facecolor(color)
+
 
 plt.xticks(np.arange(1, len(nbThreads) + 1), nbThreads)
 
-plt.title("Time Performance Metrics [{}.c] vs. Number of Threads".format(program_name))
+plt.title("Time Performance Metrics [{}.c] vs. Number of Threads".format(name_file))
 plt.xlabel('Number of Threads []')
-plt.ylabel('Execution Time [{}]'.format(unity_time))
+plt.ylabel('Execution Time [s]')
 plt.ylim(0)
 plt.legend()
 plt.grid()
-plt.savefig("graphs/png/" + csv_file_name + ".png")
-plt.savefig("graphs/pdf/" + csv_file_name + ".pdf")
+plt.savefig("graphs/png/perfs_graph_" + name_file + ".png")
+plt.savefig("graphs/pdf/perfs_graph_" + name_file + ".pdf")
+plt.show()
+
+
+
+plt.figure()
+
+labels = ["test-and-set", "test-and-test-and-set", "backoff-test-and-test-and-set"]
+
+for i in range(3):
+    means = []
+    for j in range(len(datas[i][0])):
+        mean = 0
+        for k in range(len(datas[i])):
+            mean += datas[i][k][j]
+        means.append(mean / len(datas[i]))
+    plt.plot(nbThreads.values, means, label=labels[i])
+
+plt.title("Time Performance Metrics [{}.c] vs. Number of Threads".format(name_file))
+plt.xlabel('Number of Threads []')
+plt.ylabel('Execution Time [s]')
+plt.ylim(0)
+plt.legend()
+plt.grid()
 plt.show()
