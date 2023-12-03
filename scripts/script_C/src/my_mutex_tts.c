9f508943f58ae4d00bc182a2e6448258f8607e05
Mathis003 <mathis.delsart@student.uclouvain.be>
2023-12-03 18:42:51 +0100

    Change TTS Mutex

--- scripts/script_C/src/my_mutex_tts.c
+++ scripts/script_C/src/my_mutex_tts.c
@@ -10,30 +10,28 @@ void my_mutex_destroy(my_mutex_t *my_mutex)
     my_mutex = NULL; // Make my_mutex unusable
 }
 
+void exch(volatile int *lock_value, int *result)
+{
+    asm volatile("xchg %0, %1\n\t"
+                 :"+m" (*lock_value), "+r" (*result));
+}
+
 int my_mutex_lock(my_mutex_t *my_mutex)
 {
-    int result;
-    asm volatile("while_locked:\n\t"     // label "while_locked"
-                 "testl %1, %0\n\t"      // AND operation => set flag to the result
-                 "jnz while_locked\n\t"  // flag != 0 => Jump label "while_locked"
+    while (my_mutex->lock == 1) {};
 
-                 "enter:\n\t"            // label "enter"
-                 "xchg %0, %1\n\t"       // Exchange my_mutex->lock with 1 (atomic instruction)
-                 "testl %1, %1\n\t"      // AND operation => set flag to the result
-                 "jnz enter\n\t"         // flag != 0 => Jump label "enter"
-
-                 :"=m"(my_mutex->lock), "=a"(result)  // %0 = my_mutex->lock, %1 = result
-                 :"a"(1), "m"(my_mutex->lock)         // Initialization : %1 set to 1, %0 set to my_mutex->lock
-    );
+    int result = 1;
+    while (1)
+    {
+        exch(&(my_mutex->lock), &result);
+        if (result != 1) break;
+    }
     return result;
 }
 
 int my_mutex_unlock(my_mutex_t* my_mutex)
 {
-    int result;
-    asm volatile("xchg %0, %1\n\t"  // Exchange my_mutex->lock with 0 (atomic instruction)
-                 :"=m"(my_mutex->lock), "=a"(result)  // %0 = my_mutex->lock, %1 = result
-                 :"a"(0), "m"(my_mutex->lock)         // Initialization : %1 set to 0, %0 set to my_mutex->lock
-    );
+    int result = 0;
+    exch(&(my_mutex->lock), &result);
     return result;
 }
